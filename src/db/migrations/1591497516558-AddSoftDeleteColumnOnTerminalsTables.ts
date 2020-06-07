b35ca029d95995465d2d67474dd59c47c5391c72
thecodearcher <brian.iyoha@gmail.com>
2020-06-07 03:50:22 +0100

    fix module name typo

--- src/db/migrations/1591497516558-AddSoftDeleteColumnOnTerminalsTables.ts
+++ src/db/migrations/1591497516558-AddSoftDeleteColumnOnTerminalsTables.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm-plus";
 
 export class AddSoftDeleteColumnOnTerminalsTables1591497516558 implements MigrationInterface {
     public name = "AddSoftDeleteColumnOnTerminalsTables1591497516558";
