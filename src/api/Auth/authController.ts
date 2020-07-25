caa7c733980ef7ceec59721c7c92270587a5ae2f
thecodearcher <brian.iyoha@gmail.com>
2020-07-25 22:13:49 +0100

    Revert "debug"
    This reverts commit 6c33eae2a2679692db49ed8f688dda442739616d.

--- src/api/Auth/authController.ts
+++ src/api/Auth/authController.ts
@@ -1,5 +1,3 @@
-import { Request } from "express";
-import { logger } from "./../../utils/logger";
 import { SignupData } from "./authInterface";
 import { AuthService } from "./authService";
 import { BaseController } from "../baseController";
@@ -7,8 +5,7 @@ import { BaseController } from "../baseController";
 export class AuthController extends BaseController {
   private authService = new AuthService();
 
-  public login = async (body: { phoneNumber: string; password: string; req: Request; }) => {
-    logger.info(body.req.ip);
+  public login = async (body: { phoneNumber: string; password: string; }) => {
     const data = await this.authService.loginUser(body);
     return this.sendResponse({ data, message: "Logged In" });
   }
